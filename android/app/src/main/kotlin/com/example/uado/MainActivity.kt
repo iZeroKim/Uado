d62a1dd136aa400ea0d6003c76235cc82e853348
iZeroKim <kimgichuru6@gmail.com>
2022-11-21 12:17:15 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/uado/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uado
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/auth/login_initial_page.dart
@@ -0,0 +1,28 @@
+import "package:flutter/material.dart";
+
+class InitialLoginPage extends StatefulWidget {
+  const InitialLoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<InitialLoginPage> createState() => _InitialLoginPageState();
+}
+
+class _InitialLoginPageState extends State<InitialLoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          // App logo
+
+          //Welcome text
+          //Login with text
+          //Row to hold facebook and google buttons
+          //Or text
+          //Email login button
+          //Row/ Container for register link
+        ],
+      ),
+    );
+  }
+}
