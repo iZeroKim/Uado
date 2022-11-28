d87b8767c73cf26ffcc84c1ab5efc9a1c5c8f06b
iZeroKim <kimgichuru6@gmail.com>
2022-11-28 12:43:43 +0300

    Indicate get mechanics loading progress

--- lib/providers/mechanic_provider.dart
+++ lib/providers/mechanic_provider.dart
@@ -4,14 +4,19 @@ import 'package:uado/models/Mechanic.dart';
 
 class MechanicProvider extends ChangeNotifier{
   final MechanicServices _mechanicServices = MechanicServices();
+  bool isLoading = false;
 
   List<Mechanic> _mechanics = [];
   List<Mechanic> get mechanics => _mechanics;
 
   Future<void> getAllMechanics() async{
+    isLoading = true;
+    notifyListeners();
+
     final response = await _mechanicServices.getAllMechanics();
 
     _mechanics = response;
+    isLoading = false;
     notifyListeners();
   }
 }
