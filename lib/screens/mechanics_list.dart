3f288bfd350630a270078511cc5f44bed2fb30d9
iZeroKim <kimgichuru6@gmail.com>
2022-11-28 14:43:36 +0300

    Change sort by icon, and show bottom sheet view

--- lib/screens/mechanics_list.dart
+++ lib/screens/mechanics_list.dart
@@ -128,18 +128,44 @@ class _MechanicsListPageState extends State<MechanicsListPage> {
                           ),
                         ),
                       ),
-                      Card(
-                        color: Colors.white,
-                        shape: const StadiumBorder(),
-                        elevation: 0.0,
-                        child: Container(
-                          width: 60.0,
-                          height: 60.0,
-                          child: const Center(
-                            child: Center(
-                                child: Icon(
-                                  Icons.alt_route_rounded, color: Color
-                                    .fromRGBO(78, 199, 50, 1), size: 40,)),
+                      InkWell(
+                        onTap: (){
+                          showModalBottomSheet<void>(
+                            // context and builder are
+                            // required properties in this widget
+                            context: context,
+                            builder: (BuildContext context) {
+                              // we set up a container inside which
+                              // we create center column and display text
+
+                              // Returning SizedBox instead of a Container
+                              return SizedBox(
+                                height: 200,
+                                child: Center(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: const <Widget>[
+                                      Text('Sort By'),
+                                    ],
+                                  ),
+                                ),
+                              );
+                            },
+                          );
+                        },
+                        child: Card(
+                          color: Colors.white,
+                          shape: const StadiumBorder(),
+                          elevation: 0.0,
+                          child: Container(
+                            width: 60.0,
+                            height: 60.0,
+                            child: const Center(
+                              child: Center(
+                                  child: Icon(
+                                    Icons.compare_arrows_sharp, color: Color
+                                      .fromRGBO(78, 199, 50, 1), size: 40,)),
+                            ),
                           ),
                         ),
                       ),
