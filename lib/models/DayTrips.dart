a233371c1c89331488f359d2fa26182449f9bfa9
iZeroKim <kimgichuru6@gmail.com>
2022-12-05 11:02:12 +0300

    Add DayTrip model to hold Trips in a day

---
+++ lib/models/DayTrips.dart
@@ -0,0 +1,8 @@
+import 'Trip.dart';
+
+class DayTrip{
+  final DateTime date;
+  final List<Trip> trips;
+
+  DayTrip(this.date, this.trips);
+}
---
+++ lib/services/trip_services.dart
@@ -0,0 +1,12 @@
+import '../models/Trip.dart';
+import '../models/DayTrips.dart';
+
+class TripServices {
+  //Get Trips from Api
+  Future<List<DayTrip>> getAllTrips() async {
+    List<DayTrip> dayTrips = [];
+    List<Trip> trips = [];
+
+    return dayTrips;
+  }
+}
