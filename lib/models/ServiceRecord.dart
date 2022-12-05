09766aa9aca4e7f305e4f542e2f9dad8a22471bf
iZeroKim <kimgichuru6@gmail.com>
2022-12-05 11:38:30 +0300

    Return dummy service records

--- lib/models/ServiceRecord.dart
+++ lib/models/ServiceRecord.dart
@@ -3,7 +3,7 @@ import 'Garage.dart';
 class ServiceRecord{
   final DateTime dateTime;
   final String service;
-  final String cost;
+  final int cost;
   final Garage? garage;
   final Mechanic mechanic;
   ServiceRecord( { required this.dateTime,required this.service, required this.cost, required this.garage, required this.mechanic});
---
+++ lib/services/service_records_services.dart
@@ -0,0 +1,18 @@
+import '../models/ServiceRecord.dart';
+import '../models/Mechanic.dart';
+
+class ServiceRecordsServices{
+  List<ServiceRecord> records  = [];
+
+  Future<List<ServiceRecord>> getAllRecords() async {
+    ServiceRecord record1 = ServiceRecord(
+        dateTime: DateTime(2022, 12, 5),
+        service: "Oil Change",
+        cost: 2500,
+        garage: null,
+        mechanic: Mechanic(["Oil"], "Mechanic@fb", "Mechanic@twitter", "Mechanic@linkedIn", mechanicId: "mechanic05", name: "Allan Allan", mobile: "+254701609514", gender: "Male", address: "Millimani annex, 4th street, S. Fransisco", open: "08:00", close: "19:00")
+    );
+    records.add(record1);
+    return records;
+  }
+}
