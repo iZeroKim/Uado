cd7d8632ae7afb56612084c0f4c5eec9bfd19f3c
iZeroKim <kimgichuru6@gmail.com>
2022-12-05 11:02:43 +0300

    Create dummy trips

--- lib/services/trip_services.dart
+++ lib/services/trip_services.dart
@@ -7,6 +7,12 @@ class TripServices {
     List<DayTrip> dayTrips = [];
     List<Trip> trips = [];
 
+    Trip trip1 = Trip(startTime: DateTime(2022, 12, 3, 10, 00, 00), endTime: DateTime(2022, 12, 3, 12, 00, 00), startLocation: "Elgon Court, Ralphe Bunche Road, Nairobi", endLocation: "Panorama Park Hotel, Naivasha");
+    Trip trip2 = Trip(startTime: DateTime(2022, 12, 3, 10, 00, 00), endTime: DateTime(2022, 12, 3, 12, 00, 00), startLocation: "Panorama Park Hotel, Naivasha", endLocation: "Elgon Court, Ralphe Bunche Road, Nairobi");
+
+    trips.add(trip1);
+    trips.add(trip2);
+
     return dayTrips;
   }
 }
