895fa35aecb5e26a7e1c7d1786c86d15bcb0a076
iZeroKim <kimgichuru6@gmail.com>
2022-12-05 11:29:14 +0300

    Include date in each service record

--- lib/models/ServiceRecord.dart
+++ lib/models/ServiceRecord.dart
@@ -1,10 +1,10 @@
 import 'Mechanic.dart';
 import 'Garage.dart';
 class ServiceRecord{
+  final DateTime dateTime;
   final String service;
   final String cost;
   final Garage? garage;
   final Mechanic mechanic;
-
-  ServiceRecord({required this.service, required this.cost, required this.garage, required this.mechanic});
+  ServiceRecord( { required this.dateTime,required this.service, required this.cost, required this.garage, required this.mechanic});
 }
