3d012d8e6de3922e8254770709e55fd4121c8704
iZeroKim <kimgichuru6@gmail.com>
2022-12-05 10:51:08 +0300

    Add trip model

---
+++ lib/models/Trip.dart
@@ -0,0 +1,10 @@
+class Trip{
+  final DateTime startTime;
+  final DateTime endTime;
+  final String startLocation;
+  final String endLocation;
+
+  Trip({required this.startTime, required this.endTime, required this.startLocation, required this.endLocation});
+
+
+}
