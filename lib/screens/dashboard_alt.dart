01d6d39d9e70a8e518d687add1c94b34fd37ba48
iZeroKim <kimgichuru6@gmail.com>
2022-12-07 13:35:05 +0300

    Add latest transactions tab

--- lib/screens/dashboard_alt.dart
+++ lib/screens/dashboard_alt.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:uado/screens/mechanic/mechanic_location.dart';
-
+import 'package:getwidget/getwidget.dart';
 import 'package:uado/screens/clubs/club_list.dart';
 import 'package:uado/screens/garage/garage_list.dart';
 import 'package:uado/screens/services/services.dart';
@@ -66,139 +65,208 @@ class _DashboardAltState extends State<DashboardAlt> {
         ],
       ),
       body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    color: const Color.fromRGBO(24, 158, 138, 1),
-                    elevation: 2,
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                          top: 20.0, bottom: 20.0, right: 10.0, left: 10.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.all(10.0),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: const [
-                                    Text('Primary Car :',
-                                        style: TextStyle(
-                                            color: Color.fromRGBO(254, 225, 132, 1),
-                                          fontWeight: FontWeight.bold,
-                                            fontSize: 16.0,)),
-                                    Padding(
-                                      padding: EdgeInsets.only( top:3.0, bottom: 10.0),
-                                      child: Text(
-                                        'Mercedes W205',
-                                        style: TextStyle(
-                                            color: Color.fromRGBO(254, 225, 132, 1), fontSize: 36,fontWeight: FontWeight.bold),
-                                      ),
-                                    ),
-
-                                  ],
-                                ),
-                              ),
-                              // const SizedBox(
-                              //   height: 50,
-                              //   width: 50,
-                              //   child: Image(
-                              //     image: AssetImage("assets/images/logo.png"),
-                              //     fit: BoxFit.fill,
-                              //   ),
-                              // ),
-                            ],
-                          ),
-                          // Row(
-                          //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          //   children: [
-                          //     Padding(
-                          //       padding: const EdgeInsets.only(left: 10.0),
-                          //       child: Text('Last Service : ',
-                          //           style: TextStyle(
-                          //             color: Color.fromRGBO(254, 225, 132, 1),
-                          //             fontSize: 18.0,
-                          //           fontWeight: FontWeight.bold)),
-                          //     ),
-                          //     Text('04/12/2022',
-                          //         style: TextStyle(
-                          //           color: Color.fromRGBO(254, 225, 132, 1),
-                          //           fontSize: 18.0,
-                          //             fontWeight: FontWeight.bold)),
-                          //   ],
-                          // ),
-                        ],
+        child: ListView(
+          children:[ Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
                       ),
-                    ),
-                  ),
-                  Padding(padding: EdgeInsets.only(top: 20.0, left: 20),
-                    child: Text("Shortcuts", style: TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),),
-                  )
-                ],
-              ),
-            ),
-            Padding(padding: EdgeInsets.symmetric(horizontal: 10.0),
-              child: Container(
-                height: 100,
-                child: Expanded(
-                  child: ListView.builder(
-                    itemCount: services.length,
-                    shrinkWrap: true,
-                    physics: ScrollPhysics(),
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (BuildContext context, int index) {
-                      var service = services[index];
-                      return Padding(
-                          padding: const EdgeInsets.all(5),
-                          child: InkWell(
-                            onTap: () {
-                              service.link != null
-                                  ? Navigator.of(context).push(
-                                  MaterialPageRoute(
-                                      builder: (context) => service.link!))
-                                  : print("Null");
-                            },
-                            child: Card(
-                              semanticContainer: true,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10.0),
-                                ),
-                              clipBehavior: Clip.antiAlias,
-                              child: Container(
-                                width: 90,
-                                child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                    children: <Widget>[
-                                      Icon(Icons.ac_unit_sharp, color: Color.fromRGBO(24, 158, 138, 1), size: 32,),
-                                      Text(
-                                        service.name,
+                      color: const Color.fromRGBO(24, 158, 138, 1),
+                      elevation: 2,
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            top: 20.0, bottom: 20.0, right: 10.0, left: 10.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: const [
+                                      Text('Primary Car :',
+                                          style: TextStyle(
+                                              color: Color.fromRGBO(254, 225, 132, 1),
+                                            fontWeight: FontWeight.bold,
+                                              fontSize: 16.0,)),
+                                      Padding(
+                                        padding: EdgeInsets.only( top:3.0, bottom: 10.0),
+                                        child: Text(
+                                          'Mercedes W205',
+                                          style: TextStyle(
+                                              color: Color.fromRGBO(254, 225, 132, 1), fontSize: 36,fontWeight: FontWeight.bold),
+                                        ),
                                       ),
+
                                     ],
                                   ),
-                              ),
+                                ),
+                                // const SizedBox(
+                                //   height: 50,
+                                //   width: 50,
+                                //   child: Image(
+                                //     image: AssetImage("assets/images/logo.png"),
+                                //     fit: BoxFit.fill,
+                                //   ),
+                                // ),
+                              ],
                             ),
-                          ));
-                    },
+                            // Row(
+                            //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            //   children: [
+                            //     Padding(
+                            //       padding: const EdgeInsets.only(left: 10.0),
+                            //       child: Text('Last Service : ',
+                            //           style: TextStyle(
+                            //             color: Color.fromRGBO(254, 225, 132, 1),
+                            //             fontSize: 18.0,
+                            //           fontWeight: FontWeight.bold)),
+                            //     ),
+                            //     Text('04/12/2022',
+                            //         style: TextStyle(
+                            //           color: Color.fromRGBO(254, 225, 132, 1),
+                            //           fontSize: 18.0,
+                            //             fontWeight: FontWeight.bold)),
+                            //   ],
+                            // ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 20.0, left: 20),
+                      child: Text("Shortcuts", style: TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),),
+                    )
+                  ],
+                ),
+              ),
+              Padding(padding: EdgeInsets.symmetric(horizontal: 10.0),
+                child: Container(
+                  height: 100,
+                  child: Expanded(
+                    child: ListView.builder(
+                      itemCount: services.length,
+                      shrinkWrap: true,
+                      physics: ScrollPhysics(),
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (BuildContext context, int index) {
+                        var service = services[index];
+                        return Padding(
+                            padding: const EdgeInsets.all(5),
+                            child: InkWell(
+                              onTap: () {
+                                service.link != null
+                                    ? Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                        builder: (context) => service.link!))
+                                    : print("Null");
+                              },
+                              child: Card(
+                                semanticContainer: true,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                  ),
+                                clipBehavior: Clip.antiAlias,
+                                child: Container(
+                                  width: 90,
+                                  child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                      children: <Widget>[
+                                        Icon(Icons.ac_unit_sharp, color: Color.fromRGBO(24, 158, 138, 1), size: 32,),
+                                        Text(
+                                          service.name,
+                                        ),
+                                      ],
+                                    ),
+                                ),
+                              ),
+                            ));
+                      },
+                    ),
                   ),
                 ),
               ),
-            )
+              Container(
+                color: Colors.white,
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(padding: EdgeInsets.only(top: 20.0, left: 20, bottom: 10.0),
+                      child: Text("Latest Transactions", style: TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),),
+                    ),
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            height: 80,
+                            child: const GFListTile(
+                                avatar: CircleAvatar(
+                                  minRadius: 40,
+                                  backgroundImage: AssetImage("assets/images/avatar.jpg"),
+                                ),
+                                titleText: 'Oil change',
+                                subTitleText: 'John Doe'),
+                          ),
+                        ),
+                        const Divider(
+                          thickness: 0.5,
+                          indent: 0,
+                          endIndent: 0,
+                          color: Colors.grey,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            height: 80,
+                            child: const GFListTile(
+                                avatar: CircleAvatar(
+                                  minRadius: 40,
+                                  backgroundImage: AssetImage("assets/images/avatar.jpg"),
+                                ),
+                                titleText: 'Oil change',
+                                subTitleText: 'John Doe'),
+                          ),
+                        ),
+                        const Divider(
+                          thickness: 0.5,
+                          indent: 0,
+                          endIndent: 0,
+                          color: Colors.grey,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            height: 80,
+                            child: const GFListTile(
+                                avatar: CircleAvatar(
+                                  minRadius: 40,
+                                  backgroundImage: AssetImage("assets/images/avatar.jpg"),
+                                ),
+                                titleText: 'Oil change',
+                                subTitleText: 'John Doe'),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              )
 
 
 
-          ],
+            ],
 
+          )],
         ),
       ),
     );
--- lib/screens/mechanic/mechanic_location.dart
+++ lib/screens/mechanic/mechanic_location.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+// import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 
 class MechanicLocation extends StatefulWidget {
   const MechanicLocation({Key? key}) : super(key: key);
@@ -11,16 +11,16 @@ class MechanicLocation extends StatefulWidget {
 class _MechanicLocationState extends State<MechanicLocation> {
   @override
   Widget build(BuildContext context) {
-    MapController controller = MapController(
-      initMapWithUserPosition: false,
-      initPosition: GeoPoint(latitude: 47.4358055, longitude: 8.4737324),
-      areaLimit: BoundingBox(
-        east: 10.4922941,
-        north: 47.8084648,
-        south: 45.817995,
-        west: 5.9559113,
-      ),
-    );
+    // MapController controller = MapController(
+    //   initMapWithUserPosition: false,
+    //   initPosition: GeoPoint(latitude: 47.4358055, longitude: 8.4737324),
+    //   areaLimit: BoundingBox(
+    //     east: 10.4922941,
+    //     north: 47.8084648,
+    //     south: 45.817995,
+    //     west: 5.9559113,
+    //   ),
+    // );
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color.fromRGBO(24, 158, 138, 1),
@@ -28,47 +28,48 @@ class _MechanicLocationState extends State<MechanicLocation> {
         title: const Text("Located at"),
       ),
       body: SafeArea(
-          child: OSMFlutter(
-            controller: controller,
-            trackMyPosition: true,
-            initZoom: 12,
-            minZoomLevel: 8,
-            maxZoomLevel: 14,
-            stepZoom: 1.0,
-            userLocationMarker: UserLocationMaker(
-              personMarker: const MarkerIcon(
-                icon: Icon(
-                  Icons.location_history_rounded,
-                  color: Colors.red,
-                  size: 48,
-                ),
-              ),
-              directionArrowMarker: const MarkerIcon(
-                icon: Icon(
-                  Icons.double_arrow,
-                  size: 48,
-                ),
-              ),
-            ),
-            roadConfiguration: RoadConfiguration(
-              startIcon: const MarkerIcon(
-                icon: Icon(
-                  Icons.person,
-                  size: 64,
-                  color: Colors.brown,
-                ),
-              ),
-              roadColor: Colors.yellowAccent,
-            ),
-            markerOption: MarkerOption(
-                defaultMarker: const MarkerIcon(
-                  icon: Icon(
-                    Icons.person_pin_circle,
-                    color: Colors.blue,
-                    size: 56,
-                  ),
-                )),
-          )),
+          // child: OSMFlutter(
+          //   controller: controller,
+          //   trackMyPosition: true,
+          //   initZoom: 12,
+          //   minZoomLevel: 8,
+          //   maxZoomLevel: 14,
+          //   stepZoom: 1.0,
+          //   userLocationMarker: UserLocationMaker(
+          //     personMarker: const MarkerIcon(
+          //       icon: Icon(
+          //         Icons.location_history_rounded,
+          //         color: Colors.red,
+          //         size: 48,
+          //       ),
+          //     ),
+          //     directionArrowMarker: const MarkerIcon(
+          //       icon: Icon(
+          //         Icons.double_arrow,
+          //         size: 48,
+          //       ),
+          //     ),
+          //   ),
+          //   roadConfiguration: RoadConfiguration(
+          //     startIcon: const MarkerIcon(
+          //       icon: Icon(
+          //         Icons.person,
+          //         size: 64,
+          //         color: Colors.brown,
+          //       ),
+          //     ),
+          //     roadColor: Colors.yellowAccent,
+          //   ),
+          //   markerOption: MarkerOption(
+          //       defaultMarker: const MarkerIcon(
+          //         icon: Icon(
+          //           Icons.person_pin_circle,
+          //           color: Colors.blue,
+          //           size: 56,
+          //         ),
+          //       )),
+          // )),
+    child: Text("Map"),)
     );
   }
 }
