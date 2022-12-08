2101f3f83b065c1ec991e0fc7f73fac7ed94c359
iZeroKim <kimgichuru6@gmail.com>
2022-12-08 10:39:30 +0300

    Add nullable reviews to car part model

--- lib/models/Part.dart
+++ lib/models/Part.dart
@@ -1,10 +1,11 @@
-
+import 'Review.dart';
 class Part{
   final String name, brand, seller;
   final int cost;
   final Map<String, String> part_description;
+  final Review? review;
 
-  Part({required this.name, required this.brand, required this.seller, required this.cost, required this.part_description});
+  Part(this.review, {required this.name, required this.brand, required this.seller, required this.cost, required this.part_description});
 
 
 }
