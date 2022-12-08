395808553de3444ccbc6e9b2a5e534507997f3e1
iZeroKim <kimgichuru6@gmail.com>
2022-12-08 13:56:51 +0300

    Add part reviews and rating

--- lib/screens/parts/part_details.dart
+++ lib/screens/parts/part_details.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
+
 
 class PartDetails extends StatefulWidget {
   const PartDetails({Key? key}) : super(key: key);
@@ -8,6 +10,8 @@ class PartDetails extends StatefulWidget {
 }
 
 class _PartDetailsState extends State<PartDetails> {
+  var _rating = 4.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,6 +49,7 @@ class _PartDetailsState extends State<PartDetails> {
       body: ListView(
         children: [
           Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 height: 350,
@@ -58,20 +63,77 @@ class _PartDetailsState extends State<PartDetails> {
                   ),
                 ),
               ),
+              Padding(padding: EdgeInsets.symmetric(vertical:10.0),
+                child:  Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Card(
+                      color: const Color.fromRGBO(24, 158, 138, 1),
+                      shape: const StadiumBorder(),
+                      child: Container(
+                        width: 10.0,
+                        height: 10.0,
+                        child: const Center(
+                          child: Center(),
+                        ),
+                      ),
+                    ),
+                    Card(
+                      color: Colors.white,
+                      shape: const StadiumBorder(),
+                      child: Container(
+                        width: 10.0,
+                        height: 10.0,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: const Color.fromRGBO(24, 158, 138, 1),
+                            width: 1.0,
+                          ),
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: const Center(
+                          child: Center(),
+                        ),
+                      ),
+                    ),
+                    Card(
+                      color: Colors.white,
+                      shape: const StadiumBorder(),
+                      child: Container(
+                        width: 10.0,
+                        height: 10.0,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: const Color.fromRGBO(24, 158, 138, 1),
+                            width: 1.0,
+                          ),
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: const Center(
+                          child: Center(),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const Divider(
+                height: 5,
+                thickness: 0.5,
+                indent: 0,
+                endIndent: 0,
+                color: Colors.grey,
+              ),
               Padding(
-                  padding: const EdgeInsets.all(10.0),
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: const [
                       Text(
-                        "Subaru Headlights\n",
+                        "Subaru Headlights",
                         style: TextStyle(fontSize: 16, color: Colors.black, fontWeight: FontWeight.bold),
                         maxLines: 2,
-                      ),
-                      Text(
-                        "Subaru levorg",
-                        style: TextStyle(color: Colors.grey,),
-                        maxLines: 1,
                       ),
                       Text(
                         "Ksh. 3500",
@@ -80,6 +142,242 @@ class _PartDetailsState extends State<PartDetails> {
                       ),
                     ],
                   )),
+
+              Container(
+                color: Color.fromRGBO(246,248,250,1),
+                height: 10.0,
+                width: double.infinity,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right:20, top:20, bottom:0),
+                child: Text(
+                  "Review and Rating",
+                  style: TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ),
+
+              Container(
+                color: Colors.white,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20, top: 10,right: 20, bottom:20),
+                      child: Row(
+                        children: [
+                          Text(
+                            "3.5/5",
+                            style: TextStyle(color: Colors.black, fontSize: 24),
+                            textAlign: TextAlign.start,
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              //Rating
+                              Row(
+                                children: [
+                                  GFRating(
+                                    color: Color.fromRGBO(255, 191, 27, 1),
+                                    borderColor:
+                                    Color.fromRGBO(255, 191, 27, 1),
+                                    size: 20,
+                                    value: _rating,
+                                    onChanged: (value) {
+                                      // setState(() {
+                                      //   _rating = value;
+                                      // });
+                                    },
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                height: 5.0,
+                              ),
+                              //Rating and review row
+                              Row(
+                                children: const [
+                                  Text("21 Rating"),
+                                  Text(" , "),
+                                  Text("3 Reviews")
+                                ],
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(0.0),
+                          child: Container(
+                            width: double.infinity,
+                            height: 0.3,
+                            decoration: const BoxDecoration(
+                                border: Border(
+                                  bottom:
+                                  BorderSide(width: 1.0, color: Colors.grey),
+                                )),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 15.0,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(18.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Super Cool",
+                                style: TextStyle(fontSize: 16),
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                MainAxisAlignment.spaceBetween,
+                                children: [
+                                  GFRating(
+                                    color: Color.fromRGBO(255, 191, 27, 1),
+                                    borderColor:
+                                    Color.fromRGBO(255, 191, 27, 1),
+                                    size: 20,
+                                    value: 3.0,
+                                    onChanged: (value) {},
+                                  ),
+                                  Text("Nov 30, 2022"),
+                                ],
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text("By John Doe"),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                  "100% Payment Protection, 7 days easy return in case item is defective or damaged or different from what was described.")
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(0.0),
+                          child: Container(
+                            width: double.infinity,
+                            height: 0.3,
+                            decoration: const BoxDecoration(
+                                border: Border(
+                                  bottom:
+                                  BorderSide(width: 1.0, color: Colors.grey),
+                                )),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 15.0,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(18.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Super Cool",
+                                style: TextStyle(fontSize: 16),
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                MainAxisAlignment.spaceBetween,
+                                children: [
+                                  GFRating(
+                                    color: Color.fromRGBO(255, 191, 27, 1),
+                                    borderColor:
+                                    Color.fromRGBO(255, 191, 27, 1),
+                                    size: 20,
+                                    value: 3.0,
+                                    onChanged: (value) {},
+                                  ),
+                                  Text("Nov 30, 2022"),
+                                ],
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text("By John Doe"),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                  "100% Payment Protection, 7 days easy return in case item is defective or damaged or different from what was described.")
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(0.0),
+                          child: Container(
+                            width: double.infinity,
+                            height: 0.3,
+                            decoration: const BoxDecoration(
+                                border: Border(
+                                  bottom:
+                                  BorderSide(width: 1.0, color: Colors.grey),
+                                )),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 15.0,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(18.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Super Cool",
+                                style: TextStyle(fontSize: 16),
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                MainAxisAlignment.spaceBetween,
+                                children: [
+                                  GFRating(
+                                    color: Color.fromRGBO(255, 191, 27, 1),
+                                    borderColor:
+                                    Color.fromRGBO(255, 191, 27, 1),
+                                    size: 20,
+                                    value: 3.0,
+                                    onChanged: (value) {},
+                                  ),
+                                  Text("Nov 30, 2022"),
+                                ],
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text("By John Doe"),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                  "100% Payment Protection, 7 days easy return in case item is defective or damaged or different from what was described.")
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+
             ],
           )
         ],
