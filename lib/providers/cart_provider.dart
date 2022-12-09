bb26d00c46e2fb1fb013ea6434cf428bf6a42101
iZeroKim <kimgichuru6@gmail.com>
2022-12-09 10:58:39 +0300

    Refactor Cart model into CartProvider

--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -1,25 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:uado/models/Part.dart';
-class Cart extends ChangeNotifier{
+
+class CartProvider extends ChangeNotifier {
   final List<Part> _items = [];
+
   List<Part> get items => _items;
 
   int _totalPrice = 0;
+
   int get totalPrice => _totalPrice;
 
-  void add(Part part){
+  void add(Part part) {
     _items.add(part);
     _totalPrice += part.cost;
     notifyListeners();
   }
 
-  void clear(){
+  void clear() {
     _items.clear();
     notifyListeners();
   }
 
-  void remove(Part part){
+  void remove(Part part) {
     _items.remove(part);
   }
-
 }
