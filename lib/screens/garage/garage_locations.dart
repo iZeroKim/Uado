c5855676c1415f909e09bae70561172d0b3f7cf6
iZeroKim <kimgichuru6@gmail.com>
2022-12-15 22:18:58 +0300

    Center map on Nairobi

--- lib/screens/garage/garage_locations.dart
+++ lib/screens/garage/garage_locations.dart
@@ -1,2 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
 
+class GarageLocations extends StatefulWidget {
+  const GarageLocations({Key? key}) : super(key: key);
+
+  @override
+  State<GarageLocations> createState() => _GarageLocationsState();
+}
+
+class _GarageLocationsState extends State<GarageLocations> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        FlutterMap(
+          options: MapOptions(
+            center: LatLng(-1.292066, 36.821945),
+          ),
+        ),
+
+      ],
+    );
+  }
+}
 
